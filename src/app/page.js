0634796dbfab3bd377e57b89c9b313b113e0e37d
JacoTube <jacopovicchio02@gmail.com>
2026-03-13 18:13:01 +0100

    page.js corrected

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 import Program from "@/components/Program/Program";
 import Partnership from "@/components/Partnership/Partnership"
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Program />
         <Ambassadors />
         <Partnership />
+        <Footer />
       </main>
     </>
   )
